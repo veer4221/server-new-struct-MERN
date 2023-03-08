22ca4c8d7e642150c6e6370b07d6e4a2647e4819
veer <vinay.patel@bypeopletechnologies.com>
2023-03-09 01:00:21 +0530

    fix : cors error fix

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 import express from "express";
 import { APP_PORT, MONGO_ATLAS_URL } from "./config";
 const app = express();
-import routes from "./routes"
+import routes from "./routes";
 import errorHandler from "./middlewares/errorHandler";
 import mongoose from "mongoose";
-
+import cors from "cors";
+app.use(cors());
 app.use(express.json());
-app.use("/api", routes)
-
+app.use("/api", routes);
 
 mongoose.connect(MONGO_ATLAS_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('Mongoose is connected :>>>🤘😎🤘');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Mongoose is connected :>>>🤘😎🤘");
 });
 
 app.use(errorHandler);
-app.listen(APP_PORT, () => console.log(`server is running on port ::> ${APP_PORT} 🚀❤️‍🔥🚀`))
+app.listen(APP_PORT, () => console.log(`server is running on port ::> ${APP_PORT} 🚀❤️‍🔥🚀`));
